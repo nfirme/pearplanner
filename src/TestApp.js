d44adf6eb68ae663c9f31c3e667489b37e8a54e2
elconway <49247885+elconway@users.noreply.github.com>
2021-01-12 15:34:40 -0800

    Only take courses with most recent term number
    Also added text to show where loading logo goes and only colored those terms that were selected

--- src/TestApp.js
+++ src/TestApp.js
@@ -5,7 +5,7 @@ const courseColors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
 class App extends React.Component {
     state = {
-        courseList: <div></div>
+        courseList: <h3>Loading your class data...</h3>
     }
 
     componentDidMount() {
@@ -19,22 +19,33 @@ class App extends React.Component {
         try {
             let response = await fetch(proxyUrl + targetUrl)
             if (response.ok) {
-                var json = await response.json();
-                console.log(json);
+                var n = []
+                var colorIterator = 0
+                var currentTerm = 0
+                var col = ""
+                var json = await response.json()
+                console.log(json)
                 for (var i = 0; i < json.length; i++) {
-                    json[i].courseColor = courseColors[i % courseColors.length]
-                    var n = json[i].name.split("-")
+                    if (json[i].enrollment_term_id > currentTerm) {
+                        currentTerm = json[i].enrollment_term_id
+                    }
+                    n = json[i].name.split("-")
                     json[i].courseName = n[n.length - 1]
                     n = json[i].name.split(/[\s-]/)
                     json[i].courseTitle = n.slice(0, 2).join(" ")
 
                 }
-                const res = json.map((c) =>
-                    <CourseCard 
-                    courseCode={c.courseName} 
-                    courseTitle={c.courseTitle}
-                    courseColor={c.courseColor}
-                    />);
+                const res = json.map((c) => {
+                    if (c.enrollment_term_id == currentTerm) {
+                        col = courseColors[colorIterator % courseColors.length]
+                        colorIterator++
+                        return <CourseCard 
+                        courseCode={c.courseName} 
+                        courseTitle={c.courseTitle}
+                        courseColor={col}
+                        />
+                    }
+                });
                 this.setState({courseList: res});
             }
             else {
