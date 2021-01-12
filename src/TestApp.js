ff659545206844aaff2afd91659e7d7bd947aa84
elconway <49247885+elconway@users.noreply.github.com>
2021-01-11 18:29:17 -0800

    API Call and Course Name Splicing

--- src/TestApp.js
+++ src/TestApp.js
@@ -1,63 +1,59 @@
 import React from 'react'
 import CourseCard from './components/CourseCard'
 
-const courses = [
-    {
-        courseCode: "CSC 308",
-        courseTitle: "Software Engineering I",
-        courseColor: "red"
-    },
-    {
-        courseCode: "IME 314",
-        courseTitle: "Engineering Economics",
-        courseColor: "orange"
-    },
-    {
-        courseCode: "BIO 213",
-        courseTitle: "Life Science",
-        courseColor: "yellow"
-    },
-    {
-        courseCode: "PSY 350",
-        courseTitle: "Teamwork",
-        courseColor: "green"
-    },
-    {
-        courseCode: "CSC 305",
-        courseTitle: "Individual Software Design",
-        courseColor: "blue"
-    },
-    {
-        courseCode: "CSC 430",
-        courseTitle: "Programming Languages",
-        courseColor: "purple"
+const courseColors = ["red", "orange", "yellow", "green", "blue", "purple"]
+
+class App extends React.Component {
+    state = {
+        courseList: <div></div>
     }
-]
 
-const courseList = courses.map((c) =>
-    <CourseCard 
-      courseCode={c.courseCode} 
-      courseTitle={c.courseTitle} 
-      courseColor={c.courseColor}
-    />
-);
+    componentDidMount() {
+        this.callAPI();
+        this.forceUpdate();
+    }
 
-function callAPI() {
-    console.log("Hello world");
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const targetUrl = 'https://canvas.calpoly.edu/api/v1/courses.json?access_token=15279~bUAbbgLyyiQapBK61lP8Lhz8RpoSgRLx2QajYKb8xtt44BsvONMsXxRmljb8Ds6Q'
-    fetch(proxyUrl + targetUrl)
-    .then(response => response.json())
-    .then(json => console.log(json));
-}
+    callAPI = async() => {
+        const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
+        const targetUrl = 'https://canvas.calpoly.edu/api/v1/courses.json?access_token=15279~bUAbbgLyyiQapBK61lP8Lhz8RpoSgRLx2QajYKb8xtt44BsvONMsXxRmljb8Ds6Q'
+        try {
+            let response = await fetch(proxyUrl + targetUrl)
+            if (response.ok) {
+                var json = await response.json();
+                for (var i = 0; i < json.length; i++) {
+                    json[i].courseColor = courseColors[i % courseColors.length]
+                    var n = json[i].name.split("-")
+                    var endOfCode = n.length
+                    json[i].courseName = n[n.length - 1]
+                    json[i].courseTitle = n.slice(0, n.length - 1).join(" ")
 
-function App() {
-    return (
-        <div className="App">
-            {courseList}
-            <button onClick={callAPI}>Call API</button>
-        </div>
-    );
+                }
+                const res = json.map((c) =>
+                    <CourseCard 
+                    courseCode={c.courseName} 
+                    courseTitle={c.courseTitle}
+                    courseColor={c.courseColor}
+                    />);
+                this.setState({courseList: res});
+            }
+            else {
+                console.log("ERROR IN RETRIEVING API DATA");
+            }
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    render () {
+        return (
+            <>
+            <div className="App">
+            {this.state.courseList}
+            </div>
+            </>
+        )
+    };
 }
 
 export default App;
