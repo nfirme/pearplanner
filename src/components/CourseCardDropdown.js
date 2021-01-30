6e679ab7fa9c8cecbb620e6021d85be8c8228a9b
Nick Firme <nickfirme@live.com>
2021-01-29 17:04:04 -0800

    Fixed CourseCard dropdown styling

--- src/components/CourseCardDropdown.js
+++ src/components/CourseCardDropdown.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './CourseCardDropdown.css'
 
 function CourseCardDropdown(props) {
 
@@ -16,7 +15,7 @@ function CourseCardDropdown(props) {
     );
 
     return (
-        <div className="collapse course-card-dropdown" id={props.id}>
+        <div className="collapse" id={props.id}>
             <div className="card card-body">
                 <h4>Resources</h4>
                 <ul>{listItems}</ul>
