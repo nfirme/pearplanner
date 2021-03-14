fcf4756ee7e5bb4b41810888b84a715ab5fec3af
lukemetoo <lukegreenelsh@gmail.com>
2021-03-13 19:53:33 -0800

    removed Navbar tests for build optimization

--- src/components/NavBar.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NavBar from './NavBar';
-
-it("renders NavBar component", ()=> {
-    const div = document.createElement("div");
-    ReactDOM.render(<NavBar></NavBar>, div)
-})
